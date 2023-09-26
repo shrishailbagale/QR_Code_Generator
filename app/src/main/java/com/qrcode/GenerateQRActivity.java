d316d2372403b739b30fedac853fc6075cb8cf71
Shrishail Bagale <shrishailbagale3@gmail.com>
2023-09-26 08:38:09 +0530

    first commit

--- app/src/main/java/com/qrcode/GenerateQRActivity.java
+++ app/src/main/java/com/qrcode/GenerateQRActivity.java
@@ -5,11 +5,13 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,13 +22,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.play.core.integrity.v;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -37,32 +44,30 @@ import java.util.Locale;
 public class GenerateQRActivity extends AppCompatActivity {
 
     private EditText textInput;
-    private ImageView qrCodeImageView, whatsapp, upload;
+    private ImageView qrCodeImageView;
     private ProgressDialog progressDialog;
     private FirebaseStorage storage;
     private StorageReference storageRef;
-
+    ImageView upload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_qr);
 
-        storage = FirebaseStorage.getInstance();
-        storageRef = storage.getReference();
-
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Downloading QR Code...");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.setCancelable(false);
 
-        upload = findViewById(R.id.uploadButton);
-        whatsapp = findViewById(R.id.whatsappButton);
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReferenceFromUrl("gs://qr-code-generator-c69cd.appspot.com"); // Replace with your Firebase Storage bucket URL
+
         textInput = findViewById(R.id.textInput);
+        upload = findViewById(R.id.uploadButton);
         qrCodeImageView = findViewById(R.id.qrCodeImageView);
         Button generateQRButton = findViewById(R.id.generateQRButton);
         ImageView downloadButton = findViewById(R.id.downloadButton);
-        ImageView shareButton = findViewById(R.id.shareButton);
 
         generateQRButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,45 +84,27 @@ public class GenerateQRActivity extends AppCompatActivity {
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(GenerateQRActivity.this, "Work is in Progress ", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        whatsapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(GenerateQRActivity.this, "Work is in Progress ", Toast.LENGTH_SHORT).show();
-
+                if (qrCodeImageView.getDrawable() != null) {
+                    uploadQRCodeImageToFirebase();
+                } else {
+                    Toast.makeText(GenerateQRActivity.this, "QR Code image is not available.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         downloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAndRequestPermission();
+                if (qrCodeImageView.getDrawable() != null) {
+                    // Check if QR code image is available
+                    checkAndRequestPermission();
+                } else {
+                    Toast.makeText(GenerateQRActivity.this, "QR Code image is not available.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
-    private void generateQRCode(String text) {
-        try {
-            QRCodeWriter qrCodeWriter = new QRCodeWriter();
-            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 500, 500);
-            int width = bitMatrix.getWidth();
-            int height = bitMatrix.getHeight();
-            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
-                    bitmap.setPixel(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
-                }
-            }
-
-            qrCodeImageView.setImageBitmap(bitmap);
-        } catch (WriterException e) {
-            e.printStackTrace();
-        }
-    }
     private static final int PERMISSION_REQUEST_CODE = 1001;
     private static final int PERMISSION_REQUEST_MANAGE_EXTERNAL_STORAGE = 1002;
 
@@ -165,6 +152,27 @@ public class GenerateQRActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void generateQRCode(String text) {
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 500, 500);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bitmap.setPixel(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+                }
+            }
+
+            qrCodeImageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void saveQRCodeImage() {
         qrCodeImageView.setDrawingCacheEnabled(true);
         final Bitmap qrCodeBitmap = qrCodeImageView.getDrawingCache();
@@ -226,14 +234,67 @@ public class GenerateQRActivity extends AppCompatActivity {
         }
     }
 
+    private void uploadQRCodeImageToFirebase() {
+        // Generate a unique file name or use your own naming convention
+        String fileName = "QRCode_" + System.currentTimeMillis() + ".png";
+
+        // Create a reference to the Firebase Storage location
+        StorageReference qrCodeImageRef = storageRef.child("qr_code_images/" + fileName);
+
+        // Get the Bitmap from the ImageView
+        Bitmap qrCodeBitmap = ((BitmapDrawable) qrCodeImageView.getDrawable()).getBitmap();
+
+        // Create a byte array from the Bitmap (PNG format)
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        qrCodeBitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        // Upload the image to Firebase Storage
+        UploadTask uploadTask = qrCodeImageRef.putBytes(data);
+
+        // Listen for upload success/failure
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Image upload success
+                Toast.makeText(GenerateQRActivity.this, "QR Code image uploaded to Firebase Storage.", Toast.LENGTH_SHORT).show();
+
+                // Generate and display the download link
+                qrCodeImageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String downloadUrl = uri.toString();
+                        // You can now use `downloadUrl` to share or display the link
+                        shareGeneratedLink(downloadUrl);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // Image upload failed
+                Log.e("UploadError", "Upload failed: " + e.getMessage());
+                Toast.makeText(GenerateQRActivity.this, "Failed to upload QR code image.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void shareGeneratedLink(String downloadUrl) {
+        // Create an Intent to send the download URL
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, downloadUrl);
-        startActivity(Intent.createChooser(shareIntent, "Share Download Link"));
+
+        // Create a chooser to display app options for sharing
+        Intent chooserIntent = Intent.createChooser(shareIntent, "Share Download Link");
+
+        // Check if there are apps available to handle the share intent
+        if (shareIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooserIntent);
+        } else {
+            Toast.makeText(this, "No app available to handle sharing.", Toast.LENGTH_SHORT).show();
+        }
     }
-
-
 
 
 }
